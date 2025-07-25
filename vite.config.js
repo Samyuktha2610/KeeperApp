abd802df23c41d6c6539caae98487224698a0ee1
Samyuktha2610 <samyuktha2662@gmail.com>
2025-07-26 00:14:24 +0530

    Moved files for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/KeeperApp/',
-  plugins: [react()]
+  base: '/KeeperApp/', // 👈 Your repo name here
+  plugins: [react()],
 });
